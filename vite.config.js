1ec04795c8d8cf19907bcb32b3520d9359e40a35
Tenzin M <tenzinm@Tenzins-Air-3.lan>
2024-05-15 20:51:47 -0400

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tic-tak-toe/'
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
